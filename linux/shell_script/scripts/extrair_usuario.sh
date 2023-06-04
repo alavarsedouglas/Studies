15c3809075e5c4c1b37c5579f4fbdaee146572cc
Douglas Alavarse <ds.alavarse@gmail.com>
2023-06-04 19:11:57 -0300

    Replaced if to case
    Cleaning the code using case instead if.

--- linux/shell_script/scripts/extrair_usuario.sh
+++ linux/shell_script/scripts/extrair_usuario.sh
@@ -1,27 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# extrair_usuario.sh - Busca e extrai os usuários do /etc/passwd, 
+# havendo a possibilidade de colocar em maisúsculo e em ordem alfabética
+#
+# Site: https://douglasalavarse.com/
+# Author: Douglas Alavarse
+# e-mail: alavarsedouglas@gmail.com
+#
+# ----------------------------------------------------------------------
+# Este programa recebe os dados do /etc/passwd e formata, através 
+# de manipulação de texto, para que sejam exibidos todos usuários 
+# cadastrados no passwd.
+#
+# Exemplos:
+#   $ ./extrair_usuarios.sh
+#   _douglas
+#   _alavarse
+#   _userx
+#
+# Histórico:
+#
+#   v1.0 2023-06-04, Douglas Alavarse:
+#       - Versão inicial procurando no /etc/passwd,
+#       adicionado -s, -h e -v. 
+#
+#
+# Testado em:
+#   zsh 5.9 (x86_64-apple-darwin22.0)
 
-USERS="$(cat /etc/passwd | cut -f 1 -d ":")"
+
+USERS="$(cat /etc/passwd | cut -f1 -d":")"
 
 MESSAGE="
-    $0 - [OPTIONS]
+    $(basename $0) - [OPTIONS]
 
     -h - Help menu
     -v - Program Version
     -s - Sort Output
 "
 
-VERSION="v1.0"
+VERSION="v1.2"
+CHAVE_ORDENA=0
+CHAVE_MAIUSCULO=0
 
-if [ "$1" = "-h" ]; then
-    echo "$MESSAGE" && exit 0
-fi
+case "$1" in
+    -h) echo "$MESSAGE" && exit 0      ;;
+    -v) echo "$VERSION" && exit 0      ;;
+    -s) CHAVE_ORDENA=1                 ;;
+    -m) CHAVE_MAIUSCULO=1              ;;
+     *) echo "$USERS"                  ;;
+esac 
 
-if [ "$1" = "-v" ]; then
-    echo "$VERSION" && exit 0
-fi
-
-if [ "$1" = "-s" ]; then
-    echo "$USERS" | sort && exit 0
-fi
-
-echo "$USERS"
+[ $CHAVE_ORDENA -eq 1 ] && echo "$USERS" | sort
+[ $CHAVE_MAIUSCULO -eq 1 ] && echo "$USERS" | tr [a-z] [A-Z]
